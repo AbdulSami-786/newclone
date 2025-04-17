70f0b004a98c528bc7fa73c594854b0300684ba1
AbdulSami-786 <abdulsamimyousuf0@gmail.com>
2025-04-17 18:13:06 +0500

    changesfot

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import NAVbarr from './component/navbar'
-import Footer from './component/footer'
+import NAVbarr from './component/navbar.jsx'
+import Footer from './component/Footer.jsx'
 const App = () => {
   return (
   <>
